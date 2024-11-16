c86cdfc3f66b399ea062f6da2413ec129bd7993e
jake248newman <jake248newman@gmail.com>
2024-11-16 20:58:17 +0000

    Add new user to database

--- app/src/main/java/com/example/hacksheffield9app/MainActivity.kt
+++ app/src/main/java/com/example/hacksheffield9app/MainActivity.kt
@@ -3,10 +3,7 @@ package com.example.hacksheffield9app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -14,14 +11,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.example.hacksheffield9app.ui.theme.Hacksheffield9appTheme
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
-import androidx.compose.ui.res.stringResource
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.TextFieldDefaults
@@ -30,24 +24,53 @@ import androidx.compose.material.icons.filled.Search
 import androidx.compose.ui.unit.dp
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Alignment
+import com.google.firebase.Firebase
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.IgnoreExtraProperties
+import com.google.firebase.database.database
 
 class MainActivity : ComponentActivity() {
+    var database: DatabaseReference = Firebase.database.reference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
+        //writeNewUser("jake@gmail.com", "STRING","SecurePassword", "2", "Jake")
+
         setContent {
             Hacksheffield9appTheme {
-                SearchBar()
-                FavoriteCollectionsGrid()
-                taskCard("a")
+                MyApp()
             }
         }
+    }
+
+    private fun writeNewUser(emailAddress: String? = null, gardenLayout: String? = null, password: String? = null, userId: String, username: String) {
+        val user = User(userId, emailAddress, gardenLayout, password, username)
+
+        database.child(userId).setValue(user)
+    }
+}
+
+@IgnoreExtraProperties
+data class User(
+    val userId: String? = null,
+    val email: String? = null,
+    val gardenLayout: String? = null,
+    val password: String? = null,
+    val username: String? = null)
+
+@Composable
+fun MyApp(
+) {
+    Column () {
+        SearchBar()
+        FavoriteCollectionsGrid()
     }
 }
 
 @Composable
 fun SearchBar(
-    modifier: Modifier = Modifier.offset(y = 50.dp)
+    modifier: Modifier = Modifier
 ) {
     TextField(
         value = "",
@@ -76,19 +99,15 @@ fun FavoriteCollectionsGrid(
     modifier: Modifier = Modifier
 
 ) {
-    LazyHorizontalGrid(
-        rows = GridCells.Fixed(2),
-        modifier = modifier
-    ) {
-        taskData.forEach { item ->
-
+    LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
+        items(items = taskData) { text ->
+            TaskCard(text = text)
         }
-
     }
 }
 
 @Composable
-fun taskCard(
+fun TaskCard(
     text: String,
     modifier: Modifier = Modifier
 ) {
@@ -111,3 +130,4 @@ fun taskCard(
 }
 
 private val taskData = listOf("a","b","c","d","e","f")
+
